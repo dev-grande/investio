70b36186f96ab8325da807cb87e9accabe973897
Diana Grande <granded@uci.edu>
2020-08-28 04:37:33 -0700

    changed to realtime stock api call

--- packages/backend/helpers/stock.service.js
+++ packages/backend/helpers/stock.service.js
@@ -2,20 +2,44 @@
 const axios = require("axios");
 
 const getStockPrice = (stock) => {
-    return axios({
-        "method":"GET",
-        "url":`https://investors-exchange-iex-trading.p.rapidapi.com/stock/${stock.symbol}/ohlc`,
-        "headers":{
-        "content-type":"application/octet-stream",
-        "x-rapidapi-host":"investors-exchange-iex-trading.p.rapidapi.com",
-        "x-rapidapi-key":"e0f2a9bbb0msheeae280a3d53abap1e3b4djsn9843f9bf8e19",
-        "useQueryString":true
+    return axios(
+        // {
+        // "method":"GET",
+        // "url":`https://investors-exchange-iex-trading.p.rapidapi.com/stock/${stock.symbol}/ohlc`,
+        // "headers":{
+        // "content-type":"application/octet-stream",
+        // "x-rapidapi-host":"investors-exchange-iex-trading.p.rapidapi.com",
+        // "x-rapidapi-key":"e0f2a9bbb0msheeae280a3d53abap1e3b4djsn9843f9bf8e19",
+        // "useQueryString":true
+        // }
+        // }
+        {
+            "method":"GET",
+            "url":`https://investors-exchange-iex-trading.p.rapidapi.com/stock/${stock.symbol}/book`,
+            "headers":{
+            "content-type":"application/octet-stream",
+            "x-rapidapi-host":"investors-exchange-iex-trading.p.rapidapi.com",
+            "x-rapidapi-key":"e0f2a9bbb0msheeae280a3d53abap1e3b4djsn9843f9bf8e19",
+            "useQueryString":true
+            }
         }
-        })
+        )
         .then((response)=>{
             var new_stock = {symbol: stock.symbol, quantity: Number(stock.quantity) }
-            new_stock.price = Number(Number(response.data.close.price).toFixed(2))
-            new_stock.amount = Number((Number(response.data.close.price) * new_stock.quantity).toFixed(2));
+
+            // new_stock.price = Number(Number(response.data.close.price).toFixed(2))
+            // new_stock.amount = Number((Number(response.data.close.price) * new_stock.quantity).toFixed(2));
+            var prev_close = response.data.quote.close;
+            if (response.data.quote.close === response.data.quote.latestPrice)
+            prev_close = response.data.quote.open;
+            new_stock.close = Number(Number(prev_close).toFixed(2));
+
+            new_stock.price = Number(Number(response.data.quote.latestPrice).toFixed(2))
+            new_stock.amount = Number(Number((Number(response.data.quote.latestPrice) * new_stock.quantity).toFixed(2)));
+            
+            new_stock.change = Number((Number(response.data.quote.latestPrice) - Number(new_stock.close)).toFixed(2));
+            new_stock.percent = Number(Number((new_stock.change / new_stock.price)*100).toFixed(2));
+
             return new_stock;
         })
         .catch((error)=>{
