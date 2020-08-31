6a4c7ed9ea27526e74827a07f16ddb29b8cb241b
AJ Grande <ajgrande924@gmail.com>
2020-08-31 01:36:03 -0700

    feat: containerize application

--- packages/backend/controllers/pool.js
+++ packages/backend/controllers/pool.js
@@ -1,8 +1,10 @@
 const Pool = require('pg').Pool
 
+const host = process.env['POSTGRES_HOST'] || '0.0.0.0';
+
 const pool = new Pool({
   user: 'test_user',
-  host: 'localhost',
+  host: host,
   database: 'investio',
   password: 'test_password',
   port: 5432,
